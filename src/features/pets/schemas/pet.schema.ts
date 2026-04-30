efd08a269ac549033dabc8a3014ea0a649b6cdd2
Rachel Pizane <rachel.maia@dbserver.com.br>
2026-04-30 14:40:05 -0300

    [PETCONTROL-001]: hotfix: ajustar limitação e validação do campo nome; retirar tema via teclado

--- src/features/pets/schemas/pet.schema.ts
+++ src/features/pets/schemas/pet.schema.ts
@@ -2,7 +2,7 @@ import { z } from "zod"
 import { PetStatusArray, PetStatus } from "../enums/pet-status.enum"
 
 export const petSchema = z.object({
-  name: z.string().nonempty("O nome do pet é obrigatório"),
+  name: z.string().trim().nonempty("O nome do pet é obrigatório"),
   specieId: z.uuid("A espécie é obrigatória"),
   status: z.enum(PetStatusArray, { message: "O status é obrigatório" }),
   tagsIds: z.array(z.uuid()),
